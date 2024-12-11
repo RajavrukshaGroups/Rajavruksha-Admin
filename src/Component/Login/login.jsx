0d734c17d1693f0f78b2719ef04ce09ddd39e548
technical@rajavrukshagroup.in <monica.b.raju001@gmail.com>
2024-12-12 00:21:02 +0530

    updated login

--- src/Component/Login/login.jsx
+++ src/Component/Login/login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         console.log("Email:", email);
         console.log("Password:", password);
 
-        const url = "https://admin-rajavruksha.vercel.app/login";
+        const url = "https://adminpanel-backend-ycn7.vercel.app/login";
         // const url = "http://localhost:3000/login";
         try {
             const response = await fetch(url, {
